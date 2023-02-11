2061b049d22b07beb0366844d4a95ffac5ccba7b
Ariel Jericko Gacilo <42305618+bokzgacilo@users.noreply.github.com>
2023-02-11 18:15:05 +0800

    changing db configuration

--- client/api/connection.php
+++ client/api/connection.php
@@ -1,13 +1,20 @@
 <?php
   // include('emailer.php');
 
+  // date_default_timezone_set('Asia/Manila');
+  
+  // $server = "localhost";
+  // $username = "root";
+  // $password = "";
+  // $dbname = "kipoint";
+
+  // $conn = mysqli_connect($server, $username, $password, $dbname);
   date_default_timezone_set('Asia/Manila');
   
   $server = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "kipoint";
+  $username = "u324672189_kipointadmin";
+  $password = "!^o4mKR92qpB1Ok5@zU8";
+  $dbname = "u324672189_kipoint";
 
   $conn = mysqli_connect($server, $username, $password, $dbname);
-  
 ?>
