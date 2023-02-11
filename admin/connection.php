ad9bba3912e75a861fe2d8f9379a4d05c2f3df3f
Ariel Jericko Gacilo <42305618+bokzgacilo@users.noreply.github.com>
2023-02-11 18:49:08 +0800

    url fix

--- admin/connection.php
+++ admin/connection.php
@@ -1,18 +1,18 @@
 <?php
-  // date_default_timezone_set('Asia/Manila');
-
-  // $server = "localhost";
-  // $username = "root";
-  // $password = "";
-  // $dbname = "kipoint";
-
-  // $conn = mysqli_connect($server, $username, $password, $dbname);
   date_default_timezone_set('Asia/Manila');
-  
+
   $server = "localhost";
-  $username = "u324672189_kipointadmin";
-  $password = "!^o4mKR92qpB1Ok5@zU8";
-  $dbname = "u324672189_kipoint";
+  $username = "root";
+  $password = "";
+  $dbname = "kipoint";
 
   $conn = mysqli_connect($server, $username, $password, $dbname);
+  // date_default_timezone_set('Asia/Manila');
+  
+  // $server = "localhost";
+  // $username = "u324672189_kipointadmin";
+  // $password = "!^o4mKR92qpB1Ok5@zU8";
+  // $dbname = "u324672189_kipoint";
+
+  // $conn = mysqli_connect($server, $username, $password, $dbname);
 ?>
